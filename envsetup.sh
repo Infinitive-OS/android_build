da1a3da1241908305339a793d07a409211778b16
Ricardo Cerqueira <github@cerqueira.org>
2011-11-25 15:30:36 +0000

    build: Preliminary support for IO product builds
    If building a io_* product, skip crawling the filesystem for
    AndroidProduct makefiles (and, of course, parsing their contents)
    and aim directly for the device/io/<product> device configuration
    
    Change-Id: I2a5e70dda973a6fcdbba0d5e26b35b99d3f1aea2
    
    Change how we search for IO makefiles.
    
    build: Fix fastpath code for IO_BUILD
    
     * We need to adjust to the new validations
    
    Change-Id: I9066d4f437beb9597027ee6bbb52504b5e7e84c5

--- envsetup.sh
+++ envsetup.sh
@@ -69,6 +69,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+
+    if (echo -n $1 | grep -q -e "^io_") ; then
+       IO_BUILD=$(echo -n $1 | sed -e 's/^io_//g')
+    else
+       IO_BUILD=
+    fi
+    export IO_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
