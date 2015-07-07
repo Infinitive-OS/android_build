70f31250b28afe65a52e17e1a129adc8b18f35f7
Rohanpurohit <purohit.rohan@gmail.com>
2015-07-07 11:32:53 +0200

    edit roomservice

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -42,11 +42,11 @@ except ImportError:
 DEBUG = False
 default_manifest = ".repo/manifest.xml"
 
-custom_local_manifest = ".repo/local_manifests/slim_manifest.xml"
+custom_local_manifest = ".repo/local_manifests/io_manifest.xml"
 custom_default_revision = "lp5.0"
-custom_dependencies = "slim.dependencies"
-org_manifest = "SlimRoms"  # leave empty if org is provided in manifest
-org_display = "SlimRoms"  # needed for displaying
+custom_dependencies = "io.dependencies"
+org_manifest = "InfinitiveOS-Devices"  # leave empty if org is provided in manifest
+org_display = "InfinitiveOS-Devices"  # needed for displaying
 
 github_auth = None
 
